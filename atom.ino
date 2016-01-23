fb0b3ad38b3060bcb16c1e03ceeedc102c764f4c
crakensio <crazysarath8055@gmail.com>
2016-01-24 00:22:44 +0530

    PID values Updted Bot running Smoothly

--- atom.ino
+++ atom.ino
@@ -1,13 +1,11 @@
 #include <QTRSensors.h>
 #define NUM_SENSORS   8 
 #define TIMEOUT       2500
-#define EMITTER_PIN   2
-
+#define EMITTER_PIN 2
 int PWMA=5; //PWMA For Motor A
 int PWMB=6; //PWMA For Motor B
-int lasterror=0;
-int lms=150;
-int rms=150;
+int lms=255;
+int rms=255;
 
 
 QTRSensorsRC qtrrc((unsigned char[]) {2,3, 4, 7, 8, 9, 10, 11},
@@ -34,62 +32,42 @@ void setup()
   }
   digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
 
-  // print the calibration minimum values measured when emitters were on
-  Serial.begin(9600);
- /* for (int i = 0; i < NUM_SENSORS; i++)
-  {
-  //  Serial.print(qtrrc.calibratedMinimumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  
-  // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
-  //  Serial.print(qtrrc.calibratedMaximumOn[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  */
-  delay(1000);
 }
 
 
+int lasterror=0;
 
 void loop()
 {
+  
 unsigned int sensors[8];
 int position = qtrrc.readLine(sensors);
 
-//Serial.println(position);
 int error = (position - 3200);
 Serial.print(error);
-int lbs=100;
-int rbs= 100;
-float kp=0.1;
-float kd=5;
+int lbs=50;
+int rbs= 50;
+float kp=0.8;
+float kd=15;
 int motorspeed  = (kp*error) + (kd*(error-lasterror));
-
 lasterror =error;
 
-int m1speed= lbs + motorspeed;
-int m2speed= rbs + motorspeed;
+int m1speed= (lbs + motorspeed);
+int m2speed= (rbs - motorspeed);
 
 if(m1speed > lms){ m1speed = lms;}
 if(m2speed > rms){ m2speed = rms;}
 if(m1speed < 0){ m1speed =0;}
 if(m2speed < 0){ m2speed =0;}
-
-
-
+{
 digitalWrite(A0,HIGH);
 digitalWrite(A1,LOW);
 analogWrite(PWMA,m1speed);
 digitalWrite(A2,HIGH);
 digitalWrite(A3,LOW); 
 analogWrite(PWMB,m2speed);
-delay(100);
+}
+
 }
  
  
@@ -137,4 +115,18 @@ if (error > 0) // the line is on the right
 {
   m2speed=0;
 }
+
+int m1speed= (lbs + motorspeed);
+int m2speed= (rbs - motorspeed);
+
+if(m1speed > lms){ m1speed = lms;}
+if(m2speed > rms){ m2speed = rms;}
+if(m1speed < 0){ m1speed =0;}
+if(m2speed < 0){ m2speed =0;}
+digitalWrite(A0,HIGH);
+digitalWrite(A1,LOW);
+analogWrite(PWMA,m1speed);
+digitalWrite(A2,HIGH);
+digitalWrite(A3,LOW); 
+analogWrite(PWMB,m2speed);
 */
